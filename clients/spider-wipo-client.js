2caa56b67f6a69478de954c76535dc1b611a7e76
AfterWin <CJ_Zheng1023@hotmail.com>
2018-07-04 09:51:35 +0800

    新增批量翻译客户端，并且可以导出到excel里

--- clients/spider-wipo-client.js
+++ clients/spider-wipo-client.js
@@ -5,6 +5,8 @@ var Response = require('../utils/Response')
 var fs = require('fs')
 
 var translations = [], responses = [], exportData
+//会在DELAY_START和DELAY_END范围内取随机数延迟执行爬取
+var DELAY_START = 10, DELAY_END = 20
 /**
  * 翻译结果收集器，所有请求执行完后执行导出到EXCEL
  * @param res     Response对象实例
@@ -48,7 +50,6 @@ var line = arguments[0] || 1
 var startTime = +new Date()
 exportData = eu.parser(line)
 var words = exportData[0][4].split(' ')
-words = ['clms:人', 'abs:人民']
 var delay = 0
 for(var word of words){
     var arr = word.split(':')
@@ -60,7 +61,7 @@ for(var word of words){
     var res = new Response(key, value)
     responses.push(res)
     //设置延迟执行时间
-    delay += Math.floor(Math.random() * (20000-10000+1)+10000)
+    delay += Math.floor(Math.random() * (DELAY_END * 1000 - DELAY_START * 1000 + 1) + DELAY_START * 1000)
     var debouncer = fu.debounce(delay, spider.translateText)
     debouncer(req, res, collector)
 }
